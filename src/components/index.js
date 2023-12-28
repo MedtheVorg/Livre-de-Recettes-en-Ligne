7984dc65209f40128aa1451a92b486dc92585ded
MEDPC <mohamedlem216@gmail.com>
2023-12-28 09:07:19 +0100

    updated recipe add page

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 // import your componenets here and then export them
 import Navbar from "./Navbar";
+import ItemsList from "./ItemsList";
 // add your exported componenets here
-export { Navbar };
+export { Navbar, ItemsList };
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,9 +3,23 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1 className="text-6xl font-Mazzardh">Page not found 404</h1>
-      <Link to={"/"}>go back home</Link>
+    <div className="bg-customLightGray text-customBlack h-full flex flex-col items-center justify-center text-center p-8">
+      <h1 className="text-8xl "> 404</h1>
+      <p className="text-3xl font-bold mt-8 mb-4 md:text-4xl">
+        Oops! That page can't be found
+      </p>
+      <p className="md:w-[50%]">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et incidunt
+        molestiae perferendis? Maxime amet neque facilis alias fuga saepe
+        asperiores beatae possimus aperiam maiores voluptatem ex ipsam deserunt,
+        repellat vero!
+      </p>
+      <Link
+        to={"/"}
+        className="bg-customGreen text-white mt-4 rounded-full p-3 uppercase text-sm font-medium  duration-200 hover:text-customBlack hover:bg-customYellow"
+      >
+        back to homepage
+      </Link>
     </div>
   );
 };
