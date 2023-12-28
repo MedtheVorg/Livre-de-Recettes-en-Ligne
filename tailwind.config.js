6cebf4d47afd001d966fbf3a05ceaf48017dd0e5
ibouha <ibouhaamin@gmail.com>
2023-12-28 16:27:13 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.jsx"],
   theme: {
     extend: {
-      fontFamily: {
-        nanum: "Nanum Brush Script",
-      },
       colors: {
         customWhite: "#F2F1EC",
         customGreen: "#345333",
