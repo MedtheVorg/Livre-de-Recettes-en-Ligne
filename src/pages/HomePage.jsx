ef0afcdad1e42d8c57ef378819707fa545d5a8ca
ibouha <ibouhaamin@gmail.com>
2023-12-29 12:10:29 +0100

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import platimage from "../assets/images/2.png";
 function HomePage() {
   return (
     <>
-      <div className="bg-[url(/Home.svg)] bg-cover h-screen w-full">
-        <h1 className="text-[50px]">Hello there</h1>
+      <div>
+        <div className="w-1/2">
+          <h1 className="font-blod text-3xl md:text-5xl">
+            Welcome to our website
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+            natus. Pariatur sapiente atque esse repellendus nulla consequatur
+            consectetur autem laboriosam.
+          </p>
+          <button className="btn btn-outline border-t-cyan-400">Accent</button>
+        </div>
+        <div className="w-1/2">
+          <img src={platimage} alt="" />
+        </div>
       </div>
     </>
   );
