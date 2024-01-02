fb74b9c0c77842ad72de51bc4147f7b60698f010
unknown <hossam-dev14>
2024-01-02 15:32:35 +0100

    Fetching data from db.json

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,56 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { getRecipieById } from "../../server/apiMethods";
+import { useNavigate } from "react-router";
+import { getRecipeById } from "../../server/apiMethods";
 import dots from "../assets/6dots.svg";
-import cheescake from "../assets/cheescake.png";
 
 const RecipePage = () => {
-  const [recipie, setRecipie] = useState([]);
-
+  const [recipe, setRecipe] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    
+    getRecipeById(12).then(data => {
+        setRecipe(data);
+    })
 
-    return handleGetRecipesById();
-    
   }, [])
-
   
 
-  const handleGetRecipesById = () => {
-    
-    getRecipieById(3).then(data => console.log(data));
-  
-    // setRecipie(getRecipieById(4))
-    // // .then(data => console.log(data))
-  }
 
-  // setRecipie(getRecipieById(3))
-  // console.log(recipie);
+  // console.log(recipe.nutritionValues);
 
-  
+  // const { calories, protein } = recipe.nutritionValues;
 
   return (
     <div className="container mx-auto my-8 px-8">
       <div>
       <div className="">
         <h1 className="text-4xl my-1">{
-            recipie.title
+            recipe.title
           }</h1>
         <div className="flex justify-between flex-wrap items-center ">
             <div className="flex items-baseline gap-2">
               <img className="w-4" src={dots} alt="dots" />
               <p>{
-                recipie.description
+                recipe.description
               }</p>
             </div>
             <div className="flex gap-4">
-              <button className="bg-customGreen text-white rounded uppercase duration-200 hover:text-customBlack hover:bg-customYellow w-36 h-12 mt-2">Update Recipe</button>            
+              <button onClick= {() => navigate(`/RecipeUpdatePage/${recipe.id}`)} className="bg-customGreen text-white rounded uppercase duration-200 hover:text-customBlack hover:bg-customYellow w-36 h-12 mt-2">Update Recipe</button>            
               <button className="bg-customGreen text-white rounded uppercase duration-200 hover:text-customBlack hover:bg-customYellow w-36 h-12 mt-2">Delete Recipe</button>            
             </div>
         </div>
         </div>
         <div className="my-4 rounded">
-          <img src={cheescake} alt="cheescake"/>
+          <img className="w-full rounded" src={recipe.thumbnail} alt="image"/>
         </div>
       </div>
       <main>
@@ -59,7 +50,7 @@ const RecipePage = () => {
             <h4 className="text-3xl pb-4">Ingredients</h4>
             <div className="ingredietList">
               {
-                recipie?.ingredients?.map((e, i) => {
+                recipe?.ingredients?.map((e, i) => {
                   return (
                     <div className="text-1xl mb-3" key={i}>
                       <input type="checkbox" className="text-1xl mr-2" />
@@ -75,7 +66,7 @@ const RecipePage = () => {
               <div className="flex items-center mb-3">
                 <div className="desc">
                   <h3 className="text-yellow-500 text-xl">Nutrition</h3>
-                  <p className="text-gray-500">Per Serving: </p>
+                  {/*<p className="text-gray-500">Per Serving: </p>*/}
                 </div>
               </div>
               <div className="nutrition text-lg">
@@ -83,41 +74,47 @@ const RecipePage = () => {
                   <div className="flex mb-1.5">
                       <h6 className="text-green-900 mr-2">Calories</h6>
                       <p className="leading-[27px]">
-                        {recipie?.nutrition?.calories}
+                        {recipe?.nutritionValues?.calories.value}
+                        {recipe?.nutritionValues?.calories.unit}
                       </p>
                   </div>
                   <div className="flex mb-1.5">
                       <h6 className="text-green-900 mr-2">Carbohydrates</h6>
                       <p className="leading-[27px]">
-                        {recipie?.nutrition?.carbohydrates}
+                        {recipe?.nutritionValues?.carbohydrates.value}
+                        {recipe?.nutritionValues?.carbohydrates.unit}
                       </p>
                   </div>
                   <div className="flex mb-1.5">
                       <h6 className="text-green-900 mr-2">Protein</h6>
                       <p className="leading-[27px]">
-                        {recipie?.nutrition?.protein}
+                        {recipe?.nutritionValues?.protein.value}
+                        {recipe?.nutritionValues?.protein.unit}
                       </p>
                   </div>
                   <div className="flex mb-1.5">
                       <h6 className="text-green-900 mr-2">Fat</h6>
                       <p className="leading-[27px]">
-                        {recipie?.nutrition?.fat}
+                        {recipe?.nutritionValues?.fat.value}
+                        {recipe?.nutritionValues?.fat.unit}
                       </p>
                   </div>
                   <div className="flex mb-1.5">
                       <h6 className="text-green-900 mr-2">Sodium</h6>
                       <p className="leading-[27px]">
-                        {recipie?.nutrition?.sodium}
+                        {recipe?.nutritionValues?.sodium.value}
+                        {recipe?.nutritionValues?.sodium.unit}
                       </p>
                   </div>
                   <div className="flex mb-1.5">
                       <h6 className="text-green-900 mr-2">Fiber</h6>
                       <p className="leading-[27px]">
-                        {recipie?.nutrition?.fiber}
+                        {recipe?.nutritionValues?.fiber.value}
+                        {recipe?.nutritionValues?.fiber.unit}
                       </p>
                   </div>
                 </div>
-              </div>
+            </div> 
             </article>
           </aside>
         </div>
@@ -126,7 +123,7 @@ const RecipePage = () => {
           <h4 className="text-3xl pb-5">Instructions</h4>
           <ul>
             {
-              recipie?.instructions?.map((e, i) => {
+              recipe?.instructions?.map((e, i) => {
                 return (
                   <li className="text-1xl mb-3" key={i}><span>{i+1}</span> - {e}</li>
                 )
