13b8592a819ef56930001d481ccf976b60745802
hossamdev <hossamtoubali@gmail.com>
2024-01-04 11:26:40 +0100

    Last virsion

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Link, useNavigate } from "react-router-dom";
 import { getRecipeById, deleteRecipeById } from "../../server/apiMethods";
